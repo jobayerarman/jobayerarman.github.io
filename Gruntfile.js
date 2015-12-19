d7fba388caa85ee38c84049c55efb30ed0ef23c9
Jobayer Arman <carbonjha@gmail.com>
2015-12-19 09:18:43 +0600

    update 0.6.0
    - working project section
    - project "Hybrid Skeleton" Preview

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
 module.exports = function(grunt) {
 
 	// constants for various paths and files to be used by the task configuration
-	var BUILD_DIR      = 'dist/';
+	var BUILD_DIR       = 'dist/';
 
-	var BUILD_DIR_JS   = BUILD_DIR     + 'js/';
-	var BUILD_FILE_JS  = BUILD_DIR_JS  + 'script.js';
+	var BUILD_DIR_JS    = BUILD_DIR     + 'js/';
+	var BUILD_FILE_JS   = BUILD_DIR_JS  + 'script.js';
 	var BUILD_FILES_JS  = BUILD_DIR_JS  + '*.js';
 
-	var BUILD_DIR_CSS  = BUILD_DIR     + 'css/';
-	var BUILD_FILE_CSS = BUILD_DIR_CSS + 'style.min.css';
+	var BUILD_DIR_CSS   = BUILD_DIR     + 'css/';
+	var BUILD_FILE_CSS  = BUILD_DIR_CSS + 'style.min.css';
 	var BUILD_FILES_CSS = BUILD_DIR_CSS + '*.css';
 
 
-	var SRC_DIR        = 'src/';
+	var SRC_DIR         = 'src/';
 
-	var SRC_DIR_JS     = SRC_DIR + 'js/';
-	var SRC_DIR_CSS    = SRC_DIR + 'css/';
-	var SRC_DIR_LESS   = SRC_DIR + 'less/';
+	var SRC_DIR_JS      = SRC_DIR + 'js/';
+	var SRC_DIR_CSS     = SRC_DIR + 'css/';
+	var SRC_DIR_LESS    = SRC_DIR + 'less/';
 
-	var SRC_FILES_JS   = SRC_DIR_JS   + '*.js';
-	var SRC_FILES_CSS  = SRC_DIR_CSS  + '*.css';
-	var SRC_FILES_LESS = SRC_DIR_LESS + '*.less';
+	var SRC_FILES_JS    = SRC_DIR_JS   + '*.js';
+	var SRC_FILES_CSS   = SRC_DIR_CSS  + '*.css';
+	var SRC_FILES_LESS  = SRC_DIR_LESS + '*.less';
 
 	var AP_BROWSERS = [
 		'Android >= 4',
